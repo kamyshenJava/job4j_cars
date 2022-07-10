cdf696b60fc6b2abb4785ab4e1da74021ab6ccb0
kamyshenJava <kamyshen@tutanota.com>
2022-07-10 09:52:50 +0300

    2. Фильтры для площадок машин [#4745 #320386]

--- src/main/java/ru/job4j/cars/AdRepository.java
+++ src/main/java/ru/job4j/cars/AdRepository.java
@@ -1,7 +1,6 @@
 package ru.job4j.cars;
 
 import org.hibernate.Criteria;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -13,53 +12,54 @@ import ru.job4j.cars.model.Ad;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class AdRepository {
-
-    public List<Ad> getAdsWithin24Hours(Session session) {
-        Query query = session.createQuery("Select distinct a From Ad a where a.created >= :lastDay");
-        query.setParameter("lastDay", LocalDateTime.now().minusHours(24));
-        return query.list();
+public class AdRepository implements DefaultQuery {
+    public List<Ad> getAdsWithin24Hours(SessionFactory sf) {
+        return tx(session -> {
+            Query query = session.createQuery("Select distinct a From Ad a where a.created >= :lastDay");
+            query.setParameter("lastDay", LocalDateTime.now().minusHours(24));
+            return query.list();
+        }, sf);
     }
 
-    public List<Ad> getAdsWithPhotos(Session session) {
-        Criteria cr = session.createCriteria(Ad.class);
-        cr.add(Restrictions.isNotNull("photo"));
-        return cr.list();
+    public List<Ad> getAdsWithPhotos(SessionFactory sf) {
+        return tx(session -> {
+            Criteria cr = session.createCriteria(Ad.class);
+            cr.add(Restrictions.isNotNull("photo"));
+            return cr.list();
+        }, sf);
     }
 
-    public List<Ad> getAdsWithSpecificCarBody(Session session, String carBody) {
-        Query query = session.createQuery("Select distinct a from Ad a where a.carBody = :carBody");
-        query.setParameter("carBody", carBody);
-        return query.list();
+    public List<Ad> getAdsWithSpecificCarBody(SessionFactory sf, String carBody) {
+        return tx(session -> {
+            Query query = session.createQuery("Select distinct a from Ad a where a.carBody = :carBody");
+            query.setParameter("carBody", carBody);
+            return query.list();
+        }, sf);
     }
 
     public static void main(String[] args) {
         AdRepository adRepository = new AdRepository();
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure().build();
+
         try {
-            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-            Session session = sf.openSession();
-            session.beginTransaction();
+            final SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
 
             System.out.println("The ads within last 24 hours: ");
-            for (Ad ad : adRepository.getAdsWithin24Hours(session)) {
+            for (Ad ad : adRepository.getAdsWithin24Hours(sf)) {
                 System.out.println(ad);
             }
 
             System.out.println("The ads with photos: ");
-            for (Ad ad : adRepository.getAdsWithPhotos(session)) {
+            for (Ad ad : adRepository.getAdsWithPhotos(sf)) {
                 System.out.println(ad);
             }
 
             System.out.println("The ads with hatchback: ");
-            for (Ad ad : adRepository.getAdsWithSpecificCarBody(session, "hatchback")) {
+            for (Ad ad : adRepository.getAdsWithSpecificCarBody(sf, "hatchback")) {
                 System.out.println(ad);
             }
-
-            session.getTransaction().commit();
-            session.close();
-        }  catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             StandardServiceRegistryBuilder.destroy(registry);
